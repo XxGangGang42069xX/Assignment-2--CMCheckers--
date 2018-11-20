4f913aa8e1bd52a24301ce030cb0ea946d0f6981
hunger2K <45189000+hunger2K@users.noreply.github.com>
2018-11-19 20:08:59 -0800

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -4,4 +4,9 @@ using namespace std;
 
 int main()
 {
+
 }
+
+//prototypes the function CountJumps
+int CountJumps(int CMCheckersBoard[MAX_ARRAY_SIZE][MAX_ARRAY_SIZE],
+	int numRowsInBoard, int player, int xLocArray[], int yLocArray[]);
