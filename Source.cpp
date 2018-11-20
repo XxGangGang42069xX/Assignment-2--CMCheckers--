fd30d74f73a398343290dc498ed289ed8c02717a
hunger2K <45189000+hunger2K@users.noreply.github.com>
2018-11-19 20:19:25 -0800

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -10,3 +10,24 @@ int main()
 //prototypes the function CountJumps
 int CountJumps(int CMCheckersBoard[MAX_ARRAY_SIZE][MAX_ARRAY_SIZE],
 	int numRowsInBoard, int player, int xLocArray[], int yLocArray[]);
+
+//define the function CountJumps
+int CountJumps(int CMCheckersBoard[MAX_ARRAY_SIZE][MAX_ARRAY_SIZE],
+	int numRowsInBoard, int player, int xLocArray[], int yLocArray[])
+{
+	//initialize all elements of yLocArray and xLocaray to -1
+	int xLocArray[1];
+	int yLocArray[1];
+	int index;
+	
+	for (index = 0; index < ; index++)
+	{
+		xLocArray[] = -1;
+	}
+	for (index = 0; index < ; index++)
+	{
+		yLocArray[] = -1;
+	}
+}
+	
+	
