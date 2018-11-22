baee77818452b2788036d9373cc4b40daa32cdc9
elipaniniplanas <43528404+elipaniniplanas@users.noreply.github.com>
2018-11-22 13:32:12 -0800

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -7,9 +7,26 @@ Authors: Nicholas Hung, Mark Cheng, Eli Planas
 
 using namespace std;
 
+//Global variables
+const int MAX_ARRAY_SIZE = 18;
+const int MIN_ARRAY_SIZE = 8;
+const int MAX_PIECES = 72;
+const int NOPLAYER = 0;
+const int WHITEWINS = 1;
+const int REDWINS = 2;
+const int NOONEWINS = 0;
+const int WHITESOLDIER = 1;
+const int WHITEMULE = 2;
+const int WHITEKING = 3;
+const int REDSOLDIER = 4;
+const int REDMULE = 5;
+const int REDKING = 6;
+const int WHITEPLAYER = 1;
+const int REDPLAYER = 2;
+
 int main()
 {
-	Char
+	int [y][x];
 }
 
 //prototypes the function CountJumps :: fMove it above the int main after
